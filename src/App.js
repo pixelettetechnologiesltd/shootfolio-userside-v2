c60e41a07c2740a304bbf7f12464b22da46b4a44
Ishmam Ali <ishmamali2200@gmail.com>
2023-08-05 14:50:03 -0700

    latest build

--- src/App.js
+++ src/App.js
@@ -34,29 +34,29 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
-        <Route path="/portfolio" element={<Portfolio />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
         <Route element={<ProtectedloginRoutes />}>
           <Route path="/signin" element={<Login />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="/forget" element={<Forget />}></Route>
         </Route>
-        <Route path="/performance" element={<Performance />}></Route>
         <Route element={<ProtectedRoutes />}>
+          <Route path="/portfolio" element={<Portfolio />}></Route>
           <Route path="/gamehome" element={<Gamehome />}></Route>
           <Route path="/add-payment-card" element={<Stripe />} />
+          <Route path="/playgame" element={<Playgame />}></Route>
+          <Route path="/gamemode/:id?" element={<Gamemode />}></Route>
+          <Route path="/joinleague/:id?" element={<Joinleague />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/joinclub/:id?" element={<Joinclub />}></Route>
+          <Route path="/competeclub/:id?" element={<CompeteClub />}></Route>
+          <Route path="/play/:id?" element={<Play />}></Route>
+          <Route path="/performance" element={<Performance />}></Route>
+          <Route
+            path="/portfoliocreation/:id?"
+            element={<Portfoliocreation />}
+          ></Route>
         </Route>
-        <Route path="/playgame" element={<Playgame />}></Route>
-        <Route path="/gamemode/:id?" element={<Gamemode />}></Route>
-        <Route path="/joinleague/:id?" element={<Joinleague />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-        <Route
-          path="/portfoliocreation/:id?"
-          element={<Portfoliocreation />}
-        ></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/joinclub/:id?" element={<Joinclub />}></Route>
-        <Route path="/competeclub/:id?" element={<CompeteClub />}></Route>
-        <Route path="/play/:id?" element={<Play />}></Route>
         <Route path="/not-found" element={<NotFound />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
