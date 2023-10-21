9c0e309ad21220637e9c15bdad5693b0b7634f93
Ishmam Ali <ishmamali2200@gmail.com>
2023-10-22 02:09:21 +0500

    show value in dropdown in pvm

--- src/Assets/Pages/Game/Play.js
+++ src/Assets/Pages/Game/Play.js
@@ -302,6 +302,7 @@ const Play = () => {
     const newCoinPrice = dropdownData.slice(-1);
     setNewPortfolio(coinId);
     setNewCoinPrice(newCoinPrice);
+    setCurrentCoin(coinId + " " + newCoinPrice);
   };
   const [buttonPopupMen, setButtonPopupMen] = useState(false);
   const [buttonPopupBor, setButtonPopupBor] = useState(false);
